3950fdd973ae8989b0defe93b00a69a88225deac
pjentsch <pjentsch@uwaterloo.ca>
2022-03-28 10:22:49 -0700

    some bug fixes

--- SarsEvoModel/src/SarsEvoModel.jl
+++ SarsEvoModel/src/SarsEvoModel.jl
@@ -1,6 +1,13 @@
 module SarsEvoModel
 export main
 using BenchmarkTools
+
+#Ontario population
+const initial_pop = 14.57e6
+#Size of antigenic grid
+const w = 50
+const h = 50
+
 include("antigenic_map.jl")
 include("model.jl")
 include("plotting.jl")
@@ -8,8 +15,18 @@ include("data.jl")
 
 function main()
     init_data = SarsEvoModel.load_covid_data(Date(2021, 03, 01), Date(2021, 07, 01))
+    β = Float64[0.015 * round(exp(-1 * ((i - 25)^2 / 1e3 + (j - 25)^2 / 1e3)), digits=2) for i in 1:w, j in 1:h]
+    sigma_matrix = Float64[sigma(i - k, j - l) for i in 1:w, j in 1:h, k in 1:w, l in 1:h]
 
-    sol = run(init_data, Date(2021, 03, 01))
+    params = ModelParameters(
+        β,
+        0.01,
+        0.07,
+        initial_pop,
+        0.00001,
+        sigma_matrix,
+    )
+    sol = run(init_data, Date(2021, 03, 01), params)
     plot_solution(sol)
 
     plot_antigenic_map()
--- SarsEvoModel/src/model.jl
+++ SarsEvoModel/src/model.jl
@@ -6,8 +6,6 @@ using Symbolics
 using BenchmarkTools
 using StaticArrays
 
-const w = 50
-const h = 50
 @inline function sigma(x, y; sigma_x=50.0, sigma_y=50.0, rounding=true)
     if rounding
         round(exp(-1 * ((x)^2 / sigma_x + (y)^2 / sigma_y)); digits=1)
@@ -16,8 +14,18 @@ const h = 50
     end
 end
 
+struct ModelParameters
+    β::Matrix{Float64}
+    ξ::Float64
+    γ::Float64
+    initial_population::Float64
+    M::Float64
+    sigma_matrix::Array{Float64,4}
+end
+
+
 function rhs(du, u, p, t)
-    (; β, ξ, γ, N, M, sigma_matrix) = p
+    (; β, ξ, γ, initial_population, M, sigma_matrix) = p
 
     dS = @view du[1:w, 1:h]
     dI = @view du[1:w, (h+1):(h*2)]
@@ -33,31 +41,28 @@ function rhs(du, u, p, t)
             force_of_infection = zero(eltype(sigma_matrix))
             @inbounds for l in 2:hmone
                 @inbounds for k in 2:wmone
-                    force_of_infection += sigma_matrix[k, l, i, j] * I[k, l]
+                    force_of_infection += (β[i, j] / initial_population) * sigma_matrix[k, l, i, j] * I[k, l]
                 end
             end
-            dS[i, j] = -(β[i, j] / N) * force_of_infection * S[i, j] + γ * R[i, j]
-            dI[i, j] = (β[i, j] / N) * I[i, j] * S[i, j] - ξ * I[i, j] +
+            dS[i, j] = -1 * force_of_infection * S[i, j] + γ * R[i, j]
+            dI[i, j] = (β[i, j] / initial_population) * I[i, j] * S[i, j] - ξ * I[i, j] +
                        M * (-4 * I[i, j] + I[i-1, j] + I[i+1, j] + I[i, j+1] + I[i, j-1])
             dR[i, j] = ξ * I[i, j] - γ * R[i, j]
         end
     end
 end
 
-function run(init_data, begin_date)
+function run(init_data, begin_date, params::ModelParameters)
+    (; β, sigma_matrix, initial_population) = params
 
     u0 = zeros(Float64, (w, h * 3))
 
     S = @view u0[1:w, 1:h]
     I = @view u0[1:w, (h+1):(h*2)]
     R = @view u0[1:w, (h*2+1):(h*3)]
-    initial_pop = 14.57e6
+
     #IC
-    S .= initial_pop
-
-
-
-
+    S .= initial_population
     for (lineage, (x0, y0, width), population_by_date) in init_data
         init_population_at_coords = sum(filter(:date => <=(begin_date), population_by_date).pop)
         active_population_at_coords = sum(filter(:date => x -> begin_date <= x <= begin_date + Day(5), population_by_date).pop)
@@ -69,43 +74,33 @@ function run(init_data, begin_date)
     end
 
     # TODO make this better
-    init_mrna_vaccinated = initial_pop * 1.0 #sum(filter(:date => <=(begin_date), population_by_date).pop)
-    init_az_vaccinated = initial_pop * 1.0 #sum(filter(:date => <=(begin_date), population_by_date).pop)
+    #identify strain with closest neutralization and pick that as centre for gaussian for vaccination IC
+    init_mrna_vaccinated = initial_population * 0.3 #sum(filter(:date => <=(begin_date), population_by_date).pop)
+    init_az_vaccinated = initial_population * 0.1 #sum(filter(:date => <=(begin_date), population_by_date).pop)
     mrna_vaccine = (x_i=2.7, y_i=3.8, width=30.0, pop=init_mrna_vaccinated)#B.1.1.7
     az_vaccine = (x_i=2.7, y_i=3.8, width=20.0, pop=init_az_vaccinated)
     for (; x_i, y_i, width, pop) in (mrna_vaccine, az_vaccine), x in 1:w, y in 1:h
-        S[y, x] -= sigma(x - x_i * 5, y - y_i * 5; sigma_x=width, sigma_y=width, rounding=false) .* pop
-        R[y, x] += sigma(x - x_i * 5, y - y_i * 5; sigma_x=width, sigma_y=width, rounding=false) .* pop
+        S[y, x] -= sigma(x - x_i * 5, y - y_i * 5; sigma_x=width, sigma_y=width, rounding=false) * pop
+        R[y, x] += sigma(x - x_i * 5, y - y_i * 5; sigma_x=width, sigma_y=width, rounding=false) * pop
     end
 
 
-    # return S, I, R
-    β = Float64[0.015 * round(exp(-1 * ((i - 25)^2 / 1e3 + (j - 25)^2 / 1e3)), digits=2) for i in 1:w, j in 1:h]
-    sigma_matrix = Float64[sigma(i - k, j - l) for i in 1:w, j in 1:h, k in 1:w, l in 1:h]
-    p = heatmap(1:w, 1:h, sigma_matrix[:, :, 25, 25]; plot_options...)
-    savefig(p, plots_path("sigma"))
-    p = heatmap(1:w, 1:h, β; plot_options...)
-    savefig(p, plots_path("beta"))
-
-    p = (β=β, ξ=0.01,
-        γ=0.07, N=sum(S) / length(S),
-        sigma_matrix=sigma_matrix, M=0.00001)
+    ##Plot sigma, beta, and S_0 for debugging
+    plt1 = heatmap(1:w, 1:h, sigma_matrix[:, :, 25, 25]; plot_options...)
+    savefig(plt1, plots_path("sigma"))
+    plt2 = heatmap(1:w, 1:h, β; plot_options...)
+    savefig(plt2, plots_path("beta"))
+    plt3 = heatmap(1:w, 1:h, S; plot_options...)
+    savefig(plt3, plots_path("S_0"))
 
     du0 = zeros(size(u0))
-    # @btime rhs($du0, $u0, $p, 0.0)
-    jac_sparsity = Symbolics.jacobian_sparsity((du, u) -> rhs(du, u, p, 0.0), du0, u0)
+    jac_sparsity = Symbolics.jacobian_sparsity((du, u) -> rhs(du, u, params, 0.0), du0, u0)
     f = ODEFunction(rhs; jac_prototype=float.(jac_sparsity))
 
-    prob = ODEProblem(f, u0, (0.0, 60_000.0), p)
+    prob = ODEProblem(f, u0, (0.0, 60_000.0), params)
 
     sol = solve(prob, Rodas5();
         progress=true
     )
-    #sequence diversity to interpolate additional samples??
-    #
-
-    #
-    #identify strain with closest neutralization and pick that as centre for gaussian
-    #ron fouchier, derek smith
     return sol
 end
