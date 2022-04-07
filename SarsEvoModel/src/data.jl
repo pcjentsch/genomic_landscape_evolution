1d39d2777bb4de2fe7ace151fbfb75798b62eaa6
pjentsch <pjentsch@uwaterloo.ca>
2022-04-07 15:21:55 -0700

    wew finally fixed bug in data parsing

--- SarsEvoModel/src/data.jl
+++ SarsEvoModel/src/data.jl
@@ -46,32 +46,40 @@ function load_covid_data(begin_date, end_date)
     covid_cases = CSV.File(datapath("covidtesting_on.csv")) |> DataFrame
     genomes_metadata = CSV.File(datapath("metadata_unzipped.tsv")) |> DataFrame
     genome_lineage = innerjoin(lineage_data, genomes_metadata; on=:taxon => :strain)
+    genome_lineage.date = Date.(genome_lineage.date)
     select!(genome_lineage, [:lineage, :date])
+    filter!(:lineage => !=("None"), genome_lineage)
 
+    all_lineages = unique(genome_lineage.lineage)
+    #sort by length, we want to match the longest tags preferentially
+    tags = sort(first.(antigenic_map); by=length, rev=true)
+    tag_per_lineage = map(all_lineages) do lineage
+        ind = findfirst(occursin(lineage), tags)
+        return isnothing(ind) ? nothing : tags[ind]
+    end
+    unmatched_lineages = [lineage for (tag, lineage) in zip(tag_per_lineage, all_lineages) if isnothing(tag)]
+    @show unmatched_lineages
+    lineage_tag_dict = Dict(tag => all_lineages[findall(==(tag), tag_per_lineage)] for tag in tags)
 
     cases = filter(:date => x -> begin_date <= x <= end_date, covid_cases)
-    lineage_fractions_by_date = DataFrame((date=Date(first(df).date), lineages=no_occurences(df)) for df in groupby(genome_lineage, :date))
+
+    lineage_fractions_by_date = DataFrame((date=first(df.date), lineages=no_occurences(df)) for df in groupby(genome_lineage, :date))
     cases_w_fractions = innerjoin(lineage_fractions_by_date, cases; on=:date) |> df -> sort(df, :date)
-    # before_begin_date = covid_cases[findlast(<=(begin_date), covid_cases[!, Symbol("Reported Date")]), Symbol("Total Cases")]
-    # lineage_at_begin.populations = before_begin_date .* lineage_at_begin.pop_fraction
-
-
     antigenic_landscape = map(antigenic_map) do (tag, (x, y, width))
+        matching_lineages = lineage_tag_dict[tag]
+
         cases_by_date_for_tag = map(eachrow(cases_w_fractions)) do cases_on_date
             incident_cases = cases_on_date[Symbol("Confirmed Positive")]
             date = cases_on_date.date
-            lineages = cases_on_date.lineages
+            lineages_df = cases_on_date.lineages
             pop = 0.0
             fraction = 0
-            for (; lineage, pop_fraction) in eachrow(lineages)
-
-                if occursin(tag, lineage)
-                    display((lineage, tag))
+            for (; lineage, pop_fraction) in eachrow(lineages_df)
+                if lineage in matching_lineages
                     pop += pop_fraction * incident_cases
                     fraction += pop_fraction
                 end
             end
-
             return (; date, pop, fraction)
         end
 
@@ -87,5 +95,6 @@ function load_covid_data(begin_date, end_date)
     compare_cases = innerjoin(cases, combine(antigenic_landscape_flat, :pop => sum); on=:date)
 
 
-    return cases_w_fractions, antigenic_landscape_flat, compare_cases
+    return lineage_fractions_by_date, cases_w_fractions, antigenic_landscape_flat, compare_cases
+
 end
