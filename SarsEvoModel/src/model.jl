54722d695b815b69009ea8f071589472a7d481d4
pjentsch <pjentsch@uwaterloo.ca>
2022-05-16 18:16:05 -0700

    nonlocal diffusion

--- SarsEvoModel/src/model.jl
+++ SarsEvoModel/src/model.jl
@@ -58,8 +58,9 @@ function rhs(du, u, p, t)
         end
     end
 end
-function rhs_diffusion_kernel(du, u, p, t)
-    (; params, stringency, vaccination_mrna) = p
+
+Base.Base.@propagate_inbounds function rhs_diffusion_kernel(du, u, p, t)
+    (; diffusion_kernel, params, stringency, vaccination_mrna, mrna_vaccine) = p
     (; β, ξ, γ, initial_population, M, sigma_matrix) = params
     dS = @view du[1:w, 1:h]
     dI = @view du[1:w, (h+1):(h*2)]
@@ -74,25 +75,33 @@ function rhs_diffusion_kernel(du, u, p, t)
     day = trunc(Int, t)
     stringency_t = stringency[day+1]
     yesterday_vaccinations = day >= 1 ? vaccination_mrna[day] : 0.0
-    vaccination_rate_by_day_t = 0.001 * (vaccination_mrna[day+1] - yesterday_vaccinations) / initial_population
-    @inbounds for j in 2:hmone
-        @inbounds for i in 2:wmone
+    vaccination_rate_by_day_t = (vaccination_mrna[day+1] - yesterday_vaccinations) / (initial_population * w * h)
+    for j in 2:hmone
+        for i in 2:wmone
             force_of_infection = zero(eltype(sigma_matrix))
-            @inbounds for l in 2:hmone
-                @inbounds for k in 2:wmone
+            for l in 2:hmone
+                for k in 2:wmone
                     force_of_infection += (β[i, j] / initial_population) * sigma_matrix[k, l, i, j] * I[k, l]
                 end
             end
 
+            diffusion = 0.0
+            for l in 2:hmone
+                for k in 2:wmone
+                    diffusion += I[k, l] * sigma(i - k, j - l; sigma_x=20.0, sigma_y=20.0, rounding=false)
+                end
+            end
+
             dS[i, j] = -1 * stringency_t * force_of_infection * S[i, j] + γ * R[i, j] - vaccination_rate_by_day_t * S[i, j]
-            dI[i, j] = (β[i, j] / initial_population) * stringency_t * I[i, j] * S[i, j] - ξ * I[i, j] +
-                       M * (-4 * I[i, j] + I[i-1, j] + I[i+1, j] + I[i, j+1] + I[i, j-1])
+            dI[i, j] = (β[i, j] / initial_population) * stringency_t * I[i, j] * S[i, j] - ξ * I[i, j] + M * diffusion
             dR[i, j] = ξ * I[i, j] - γ * R[i, j]
-            dV[i, j] = vaccination_rate_by_day_t * S[i, j]
+
+            x_i_transformed, y_i_transformed = map_coords_to_model_space(2.7, 3.8)
+            dV[i, j] = vaccination_rate_by_day_t * S[i, j] * sigma(i - x_i_transformed, j - y_i_transformed; sigma_x=20.0, sigma_y=20.0, rounding=false)
         end
     end
 end
-
+using OffsetArrays
 function run(location_data, begin_date, end_date, params)
     (; β, sigma_matrix, initial_population) = params
     (; dates, stringency, vaccination_mrna) = location_data
@@ -123,10 +132,11 @@ function run(location_data, begin_date, end_date, params)
         V[y, x] += sigma(x - x_i_transformed, y - y_i_transformed * 5; sigma_x=width, sigma_y=width, rounding=false) * pop
     end
 
-    diffusion_kernel = zeros(w, h) |>
+    diffusion_kernel = sigma_matrix[:, :, div(w, 2), div(h, 2)]
+    diffusion_kernel = diffusion_kernel ./ sum(diffusion_kernel)
 
-                       ##Plot sigma, beta, and S_0 for debugging
-                       plt1 = heatmap(-div(w, 2):div(w, 2), -div(h, 2):div(h, 2), sigma_matrix[:, :, div(w, 2), div(h, 2)], xlabel="antigenic distance", ylabel="antigenic distance"; plot_options...)
+    ##Plot sigma, beta, and S_0 for debugging
+    plt1 = heatmap(-div(w, 2):div(w, 2), -div(h, 2):div(h, 2), sigma_matrix[:, :, div(w, 2), div(h, 2)], xlabel="antigenic distance", ylabel="antigenic distance"; plot_options...)
     savefig(plt1, plots_path("sigma"))
     plt2 = heatmap(1:w, 1:h, β; plot_options...)
     savefig(plt2, plots_path("beta"))
@@ -134,10 +144,10 @@ function run(location_data, begin_date, end_date, params)
     savefig(plt3, plots_path("S_0"))
 
     du0 = zeros(size(u0))
-    jac_sparsity = Symbolics.jacobian_sparsity((du, u) -> rhs(du, u, (; params, stringency, vaccination_mrna), 0.0), du0, u0)
-    f = ODEFunction(rhs; jac_prototype=float.(jac_sparsity))
+    jac_sparsity = Symbolics.jacobian_sparsity((du, u) -> rhs_diffusion_kernel(du, u, (; diffusion_kernel, params, stringency, vaccination_mrna, mrna_vaccine), 0.0), du0, u0)
+    f = ODEFunction(rhs_diffusion_kernel; jac_prototype=float.(jac_sparsity))
 
-    prob = ODEProblem(f, u0, (0.0, length(stringency) - 1), (; params, stringency, vaccination_mrna))
+    prob = ODEProblem(f, u0, (0.0, length(stringency) - 1), (; diffusion_kernel, params, stringency, vaccination_mrna, mrna_vaccine))
 
     sol = solve(prob, Rodas5();
         progress=true
