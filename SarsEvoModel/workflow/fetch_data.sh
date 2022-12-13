ca95d879d6a822518b51d5c6d07ee60d7d97f155
pjentsch <pjentsch@uwaterloo.ca>
2022-12-12 22:34:26 -0800

    workflow update

--- SarsEvoModel/workflow/fetch_data.sh
+++ SarsEvoModel/workflow/fetch_data.sh
@@ -1,1 +1,10 @@
-#!/bin/bash
+!/bin/bash
+
+wget -O datasets https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets
+wget -O dataformat https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/dataformat
+
+chmod +x dataformat
+chmod +x datasets
+
+./datasets download virus genome taxon sars-cov-2 --complete-only
+unzip -p ncbi_dataset.zip ncbi_dataset/data/genomic.fna | gzip -c > all_genomes.fa.gz
